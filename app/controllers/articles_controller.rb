7da3455fd2653866637933938f57ddc55a682f7a
Brian Amaral <brianamaral918@gmail.com>
2020-03-02 08:24:26 -0300

    create flash notice,errors messages and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,17 @@ class ArticlesController < ApplicationController
 
     def create 
         @article = Article.new article_params
-        @article.save 
-        redirect_to articles_show(@article)
+        @article.save
+        if @article.save  
+            redirect_to article_path(@article)
+            flash[:notice] = "Article was sucessfully created"
+        else
+            render 'new'
+        end    
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     private
