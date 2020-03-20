6719313dd0270de5373992fe6d45fa5e0adce1b0
Brian Amaral <brianamaral918@gmail.com>
2020-03-20 19:37:47 -0300

    add secure password to users

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    validates :user_id,presence: true
     validates :title, presence: true,length: {minimum:5,maximum:30}
     validates :description, presence: true,length: {minimum:10,maximum:300}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
+    has_secure_password
+    
+    has_many :articles 
+
+    validates :username , presence: true,
+               uniqueness:{case_sensitive:false},
+    length: { minimum:3, maximum:30}
+
+    validates :email , presence: true,
+                uniqueness: true,
+                length:{minimum:5,maximum:100},
+                format:/\w+@\w+\.{1}[a-zA-Z]{2,}/
+
+
 
 end
